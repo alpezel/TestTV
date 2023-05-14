8369062f888bc98d6356f1e89004888640913791
alpezel <allistairevillaruel@gmail.com>
2023-05-14 21:53:21 +0800

    add pseudocode, class, and define functions

---
+++ TestTV.py
@@ -0,0 +1,31 @@
+# Make a Class named TV
+class TV:
+    # Create 2 objects 
+    def __init__(tv_object, channel, volume ):
+        tv_object.channel = channel
+        tv_object.volume = volume
+    # Add methods and define
+    def turnOn():None
+     
+    def turnOff():None
+    
+    def getChannel1():int
+    
+    def setChannel1(channel: int): None
+
+    def getVolume(): int
+
+    def setVolume(volumelevel: int): None
+
+    def channel1Up():None
+
+    def channel1Down():None
+
+    def volumeUp():None
+
+    def volumeDown():None
+
+
+    
+
+

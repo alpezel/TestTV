523740658dd212781827e46a614b54e7afc27624
alpezel <allistairevillaruel@gmail.com>
2023-05-15 19:04:33 +0800

    add channel and volume

--- TestTV.py
+++ TestTV.py
@@ -7,35 +7,49 @@ class TV:
 
     # Add methods and define
     def turnOn():
-        print("The TV is On")
+        print("The TV is ON")
      
     def turnOff():
-        print("The TV is On")
+        print("The TV is OFF")
     
-    def getChannel1():int
+    def getChannel():
+        for i in (channel):
+            channel_num[i] = channel_num[i] -1 
+            return channel_num                      
+
     
-    def setChannel1(channel: int): None
+    def setChannel(channel: int): None
 
     def getVolume(): int
 
     def setVolume(volumelevel: int): None
 
-    def channel1Up():None
+    def channelUp():None
 
-    def channel1Down():None
+    def channelDown():None
 
     def volumeUp():None
 
     def volumeDown():None
 
+    
+channel = list(range(1,121))
+volume = list(range(1,8))
+print("The current channel (1 to 120) of this TV.")
+print("The current volume level (1 to 7) of this TV.")
+TV.turnOff()
 while True:
-    switch = input("Do you want to turn on your TV?")
+    switch = input("Do you want to turn on your TV? ")
     while switch.lower()!= "yes" and switch.lower() != "no":
-        print ("\n[The input is not (yes or no)]")
-        switch = input("Do you want to turn on your TV?")
-        TV.turnOff() 
+        print ("\n[The input is not (yes or no)]\n")
+        switch = input("Do you want to turn on your TV? ")
+    # Indicates whether this TV is on/off
     if switch.lower() == "yes":
         TV.turnOn()
+        channel_num = int(input("What channel number you want? "))
+        TV.getChannel()
+    elif switch.lower() == "no":
+        TV.turnOff()
         break
 
 

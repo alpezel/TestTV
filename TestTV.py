07be7ef23c0651aa5d9f5205140bcf69779152e9
alpezel <allistairevillaruel@gmail.com>
2023-05-15 20:49:57 +0800

    add volume and test

--- TestTV.py
+++ TestTV.py
@@ -1,3 +1,4 @@
+#### make display menu later ####
 # Make a Class named TV
 class TV:
     # Create 2 objects 
@@ -17,12 +18,19 @@ class TV:
         if channel_num in channel:
             print("You're on channel ", channel_num)
         else:
-            print("The channel does not exist")                     
+            print("The channel does not exist")  
+        return                   
 
     
     def setChannel(channel: int): None
 
-    def getVolume(): int
+    def getVolume():
+        volume_num = int(input("What Volume Level you want? "))
+        if volume_num in volume_lvl:
+            print("Volume Level: ", volume_num)
+        else:
+            print("The volume is only up to 7")
+        return
 
     def setVolume(volumelevel: int): None
 
@@ -30,13 +38,19 @@ class TV:
 
     def channelDown():None
 
-    def volumeUp():None
+    def volumeUp(self,volume):
+        if volume in volume_lvl:
+            self.volume -= 1
+        return self.volume
 
-    def volumeDown():None
+    def volumeDown(self,volume):
+        if volume in volume_lvl:
+            self.volume -= 1
+        return self.volume
 
     
 channel = list(range(1,121))
-volume = list(range(1,8))
+volume_lvl = list(range(1,8))
 print("The current channel (1 to 120) of this TV.")
 print("The current volume level (1 to 7) of this TV.")
 #print(channel)
@@ -50,6 +64,12 @@ while True:
     if switch.lower() == "yes":
         TV.turnOn()
         TV.getChannel()
+        TV.getVolume()
+        try_vol= input("up or down")
+        if try_vol == "up":
+            TV.volumeUp()
+        else:
+            TV.volumeDown()
     elif switch.lower() == "no":
         TV.turnOff()
         break

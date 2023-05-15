566cb46ca03b108938a6db08f8499757f833f2c4
alpezel <allistairevillaruel@gmail.com>
2023-05-15 17:45:11 +0800

    test on of the tv

--- TestTV.py
+++ TestTV.py
@@ -4,10 +4,13 @@ class TV:
     def __init__(tv_object, channel, volume ):
         tv_object.channel = channel
         tv_object.volume = volume
+
     # Add methods and define
-    def turnOn():None
+    def turnOn():
+        print("The TV is On")
      
-    def turnOff():None
+    def turnOff():
+        print("The TV is On")
     
     def getChannel1():int
     
@@ -24,6 +27,16 @@ class TV:
     def volumeUp():None
 
     def volumeDown():None
+
+while True:
+    switch = input("Do you want to turn on your TV?")
+    while switch.lower()!= "yes" and switch.lower() != "no":
+        print ("\n[The input is not (yes or no)]")
+        switch = input("Do you want to turn on your TV?")
+        TV.turnOff() 
+    if switch.lower() == "yes":
+        TV.turnOn()
+        break
 
 
     
